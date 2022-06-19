007562c52d4d143c01cb1c7ecf4aec27e7e0b3e8
Lucas Paris <lucashenriqueparis@gmail.com>
2022-06-19 02:41:40 -0300

    Create the display

--- src/main/Calculator.jsx
+++ src/main/Calculator.jsx
@@ -1,4 +1,5 @@
 import { Component } from 'react'
+import Display from '../components/Display'
 import Button from '../components/Button'
 import './Calculator.css'
 
@@ -6,6 +7,7 @@ export default class Calculator extends Component{
     render(){
         return(
             <div className='calculator'>
+                <Display value={100}></Display>
                 <Button label='AC'></Button>
                 <Button label='/'></Button>
                 <Button label='7'></Button>
